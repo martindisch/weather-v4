cf806b38f18ceb819f68b37f363dc07d750d697b
Martin Disch <martindisch@gmail.com>
2022-12-19 19:03:01 +0100

    Try Pancake

--- weather-app/src/routes/+page.svelte
+++ weather-app/src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import Chart from "$lib/Chart.svelte";
   import Measurement from "$lib/Measurement.svelte";
 </script>
 
@@ -6,3 +7,4 @@
   <Measurement title="Temperature" value={21} unit="°C" />
   <Measurement title="Relative Humidity" value={60} unit="%" />
 </div>
+<Chart />
