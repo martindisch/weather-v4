ac979deadbc0a1396a5bd7f854ce62f24cc256b0
Martin Disch <martindisch@gmail.com>
2022-12-19 18:09:47 +0100

    Build measurement widget

---
+++ weather-app/src/lib/Measurement.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  export let title: string;
+  export let value: number;
+  export let unit: string;
+</script>
+
+<div>
+  <h1 class="">{title}</h1>
+  <span class="text-9xl">{value}</span><span class="text-6xl">{unit}</span>
+</div>
--- weather-app/src/routes/+page.svelte
+++ weather-app/src/routes/+page.svelte
@@ -1,1 +1,8 @@
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
+<script lang="ts">
+  import Measurement from "../lib/Measurement.svelte";
+</script>
+
+<div class="flex justify-center gap-10">
+  <Measurement title="Temperature" value={21} unit="°C" />
+  <Measurement title="Relative Humidity" value={60} unit="%" />
+</div>
