49d3285e7b816d3ba3157688d5608071e750554c
Martin Disch <martindisch@gmail.com>
2022-12-21 13:13:03 +0100

    Extract measurements into group component

---
+++ weather-app/src/lib/Measurements.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import Measurement from "$lib/Measurement.svelte";
+</script>
+
+<div class="flex justify-center gap-10">
+  <Measurement title="Temperature" value={21} unit="°C" />
+  <Measurement title="Relative Humidity" value={60} unit="%" />
+</div>
--- weather-app/src/routes/+page.svelte
+++ weather-app/src/routes/+page.svelte
@@ -1,15 +1,10 @@
 <script lang="ts">
   import Chart from "$lib/Chart.svelte";
-  import Measurement from "$lib/Measurement.svelte";
+  import Measurements from "$lib/Measurements.svelte";
 </script>
 
 <div class="h-full flex flex-col">
-  <div class="flex-1">
-    <div class="flex justify-center gap-10">
-      <Measurement title="Temperature" value={21} unit="°C" />
-      <Measurement title="Relative Humidity" value={60} unit="%" />
-    </div>
-  </div>
+  <div class="flex-1"><Measurements /></div>
   <div class="flex-1 min-h-[20em]"><Chart /></div>
   <div class="flex-1 min-h-[20em]"><Chart /></div>
 </div>
