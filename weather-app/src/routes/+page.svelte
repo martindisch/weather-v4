da19c6d847ba94db230d577f103aee7ca027b4bb
Martin Disch <martindisch@gmail.com>
2022-12-21 13:22:30 +0100

    Center widget vertically

--- weather-app/src/routes/+page.svelte
+++ weather-app/src/routes/+page.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div class="h-full flex flex-col">
-  <div class="flex-1"><Measurements /></div>
+  <div class="flex-1 flex flex-col justify-center"><Measurements /></div>
   <div class="flex-1 min-h-[20em]"><Chart /></div>
   <div class="flex-1 min-h-[20em]"><Chart /></div>
 </div>
