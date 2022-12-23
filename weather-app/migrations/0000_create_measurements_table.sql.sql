17f132635a230472285f2534e46c7d427b47f5f3
Martin Disch <martindisch@gmail.com>
2022-12-23 10:35:21 +0100

    Do migrations properly

--- weather-app/migrations/0000_create_measurements_table.sql.sql
+++ weather-app/migrations/0000_create_measurements_table.sql.sql
@@ -1,3 +1,4 @@
+-- Migration number: 0000 	 2022-12-23T09:27:53.745Z
 DROP TABLE IF EXISTS Measurements;
 CREATE TABLE Measurements (Timestamp INTEGER PRIMARY KEY ASC, Temperature Real, Humidity Real);
 INSERT INTO Measurements VALUES (1662352916,15.2,70), (1662353229,16.4,75), (1662353540,17,78.2);
