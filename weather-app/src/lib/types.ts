5033226a8a7d65f769d02e3fb28962c09d7a5cde
Martin Disch <martindisch@gmail.com>
2022-12-23 14:41:50 +0100

    Implement storing measurements

---
+++ weather-app/src/lib/types.ts
@@ -0,0 +1,5 @@
+export interface Measurement {
+  timestamp: number;
+  temperature: number;
+  humidity: number;
+}
--- weather-app/src/routes/api/measurements/+server.ts
+++ weather-app/src/routes/api/measurements/+server.ts
@@ -1,9 +1,18 @@
-import { type RequestHandler, json } from "@sveltejs/kit";
+import type { RequestHandler } from "@sveltejs/kit";
+import type { Measurement } from "$lib/types";
 
 export const POST: RequestHandler = async ({ request, platform }) => {
+  // TODO: some sort of simple authentication
+
   const db = platform.env.DB;
 
-  const { results } = await db.prepare("SELECT * FROM Measurements").all();
+  const measurements: Measurement[] = await request.json();
+  const insertStatement = db.prepare("INSERT INTO Measurements VALUES (?, ?, ?);");
+  const insertBatch = measurements.map((measurement) =>
+    insertStatement.bind(measurement.timestamp, measurement.temperature, measurement.humidity)
+  );
 
-  return json(results);
+  await db.batch(insertBatch);
+
+  return new Response(null, { status: 201 });
 };
