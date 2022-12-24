fc5423932e552176c4fa6cfadbbdd3656c4c4291
Martin Disch <martindisch@gmail.com>
2022-12-24 15:38:54 +0100

    Turn gap responsive

--- weather-app/src/lib/Measurements.svelte
+++ weather-app/src/lib/Measurements.svelte
@@ -5,7 +5,7 @@
   export let humidity: number;
 </script>
 
-<div class="flex justify-center gap-10">
+<div class="flex justify-center gap-4 md:gap-8">
   <Measurement title="Temperature" value={temperature} unit="°C" />
   <Measurement title="Relative Humidity" value={humidity} unit="%" />
 </div>
