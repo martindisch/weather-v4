8f522b32b7a4d51885b0233454110d873fb28861
Martin Disch <martindisch@gmail.com>
2022-12-27 12:41:57 +0100

    Execute inserts with wrangler

--- tools/src/main.rs
+++ tools/src/main.rs
@@ -1,10 +1,14 @@
+use eyre::{eyre, Result};
 use itertools::Itertools;
 use std::{
-    fs::File,
-    io::{BufRead, BufReader},
+    fs::{File, OpenOptions},
+    io::{BufRead, BufReader, BufWriter, Write},
+    mem,
+    process::Command,
 };
+use tempdir::TempDir;
 
-use eyre::Result;
+const DB: &str = "weather-test";
 
 fn main() -> Result<()> {
     let values = BufReader::new(File::open("history.csv")?)
@@ -12,11 +16,34 @@ fn main() -> Result<()> {
         .filter_map(|r| r.ok())
         .map(|l| format!("({l})"));
 
+    let dir = TempDir::new("weather_migration")?;
+    let file_path = dir.path().join("command.sql");
+    println!("Using temporary file {file_path:?} for SQL commands");
+
     for chunk in &values.chunks(10_000) {
         let values_joined: String =
             itertools::Itertools::intersperse(chunk, ", ".into()).collect();
 
-        println!("INSERT INTO measurements VALUES {values_joined};");
+        let mut writer = BufWriter::new(
+            OpenOptions::new()
+                .write(true)
+                .create(true)
+                .open(&file_path)?,
+        );
+        writeln!(writer, "INSERT INTO measurements VALUES {values_joined};")?;
+        mem::drop(writer);
+
+        Command::new("wrangler")
+            .arg("d1")
+            .arg("execute")
+            .arg(DB)
+            .arg("--file")
+            .arg(file_path.to_str().ok_or(eyre!("Path not unicode"))?)
+            .output()?
+            .status
+            .success()
+            .then_some(())
+            .ok_or(eyre!("Command failed"))?;
     }
 
     Ok(())
