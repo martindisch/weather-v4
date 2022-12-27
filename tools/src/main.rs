1f9e6a1982780f7ea5f9ded2ffa36fdcde5599c0
Martin Disch <martindisch@gmail.com>
2022-12-27 11:58:55 +0100

    Turn CSV into INSERT statements with 10'000 rows each

--- tools/src/main.rs
+++ tools/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use itertools::Itertools;
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+use eyre::Result;
+
+fn main() -> Result<()> {
+    let values = BufReader::new(File::open("history.csv")?)
+        .lines()
+        .filter_map(|r| r.ok())
+        .map(|l| format!("({l})"));
+
+    for chunk in &values.chunks(10_000) {
+        let values_joined: String =
+            itertools::Itertools::intersperse(chunk, ", ".into()).collect();
+
+        println!("INSERT INTO measurements VALUES {values_joined};");
+    }
+
+    Ok(())
 }
